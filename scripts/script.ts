c62927fa4edb68a39ae063d91ce94eb27e11b0bf
jdedwards3 <james.d.edwards.3@gmail.com>
2021-03-17 19:51:13 -0400

    use default export

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,9 +1,13 @@
-(async function () {
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker
-      .register("/service-worker.js", { scope: "/" })
-      .then(function () {
-        console.log("Service Worker Registered");
-      });
+export default class Main {
+  constructor() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker
+        .register("/service-worker.js", { scope: "/" })
+        .then(function () {
+          console.log("Service Worker Registered");
+        });
+    }
   }
-})();
+}
+
+new Main();
