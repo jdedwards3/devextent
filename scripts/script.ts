9c9f6984e274ae24cd3749bd3d1c68c518e17ca2
jdedwards3 <james@jamesedwards.net>
2021-06-18 15:46:30 -0400

    use default service work in production

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,13 +1,5 @@
 export default class Main {
   constructor() {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/service-worker.js", { scope: "/" })
-        .then(function () {
-          console.log("Service Worker Registered");
-        });
-    }
-
     document
       .querySelectorAll("pre")
       .forEach((item) => item.setAttribute("tabindex", "0"));
